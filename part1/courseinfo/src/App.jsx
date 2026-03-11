0fc61e6c7f5786d8202ae03f8235573120392b39
MedesPersians <156643231+MedesPersians@users.noreply.github.com>
2026-03-11 18:30:24 +0100

    General commit for part 1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const Header = props => {
+  return <h1>{props.text.name}</h1>;
+};
+
+const Content = props => {
+  return (
+    <div>
+      <Part content={props.parts[0]} />
+      <Part content={props.parts[1]} />
+      <Part content={props.parts[2]} />
+    </div>
+  );
+};
+
+const Part = props => {
+  return (
+    <p>
+      {props.content.name} {props.content.exercises}
+    </p>
+  );
+};
+
+const Total = props => {
+  return (
+    <p>
+      Number of exercises{' '}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header text={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
